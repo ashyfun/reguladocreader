675b93ebcd6506e368aca6fa997233349b8d23c1
k1n9pin <k1n9pin@yandex.ru>
2023-03-14 07:58:55 +0300

    Update CMake

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ PROG="RegulaDocumentReader"
 BINARY="$CURRENT_DIR/$BUILD_DIR/src/$PROG"
 
 rm -rf "$PROG" "$CURRENT_DIR/tmp"/*.{bmp,jpg,xml,json}
-if [ -d "$BUILD_DIR" ];
-then
+if [ -d "$BUILD_DIR" ]; then
   echo "Clear \"$BUILD_DIR\" directory"
   rm -rf "$BUILD_DIR"/*
 fi
